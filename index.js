b449f5eca7fbb5f05383c5bcb3974b73c36dd057
NathanCarr87 <carr.excel@gmail.com>
2021-01-27 09:49:42 -0700

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import stylesheets
 import "./style.css";
 import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
 // Write Javascript code!
 const appDiv = document.getElementById("app");
@@ -13,16 +14,45 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
+camera.position.set(0, 2.5, 2.5);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 const renderer = new THREE.WebGLRenderer();
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+const geometry = new THREE.PlaneGeometry(5, 20, 32);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(geometry, material);
+plane.rotateX(90);
+scene.add(plane);
 
 camera.position.z = 5;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// instantiate a loader
+const loader = new OBJLoader();
+
+// load a resource
+loader.load(
+  // resource URL
+  "assets/chr_sword.obj",
+  // called when resource is loaded
+  function(object) {
+    console.log("success", object);
+    scene.add(object);
+  },
+  // called when loading is in progresses
+  function(xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function(error) {
+    console.log("An error happened");
+  }
+);
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
