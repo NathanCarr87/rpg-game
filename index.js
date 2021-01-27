066948d68971b9e7469b2f6ac2c7776308395b6d
NathanCarr87 <carr.excel@gmail.com>
2021-01-27 13:21:43 -0700

    2 files modified

--- index.js
+++ index.js
@@ -2,65 +2,133 @@
 import "./style.css";
 import * as THREE from "three";
 // import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// author: Fyrestar <info@mevedia.com>
+var camera, scene, renderer, mesh, goal, keys, follow;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0, 2.5, 2.5);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
+var time = 0;
+var newPosition = new THREE.Vector3();
+var matrix = new THREE.Matrix4();
 
-const renderer = new THREE.WebGLRenderer();
+var stop = 1;
+var DEGTORAD = 0.01745327;
+var temp = new THREE.Vector3();
+var dir = new THREE.Vector3();
+var a = new THREE.Vector3();
+var b = new THREE.Vector3();
+var coronaSafetyDistance = 0.3;
+var velocity = 0.0;
+var speed = 0.0;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-// controls.target.set(0, 5, 0);
-// controls.update();
 
-const geometry = new THREE.PlaneGeometry(5, 20, 32);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  side: THREE.DoubleSide
-});
-const plane = new THREE.Mesh(geometry, material);
-plane.rotation.x = Math.PI * -0.5;
-scene.add(plane);
+init();
+animate();
 
-camera.position.z = 5;
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init() {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// instantiate a loader
-// const loader = new OBJLoader();
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  // const controls = new OrbitControls(camera, renderer.domElement);
 
-// load a resource
-// loader.load(
-// // resource URL
-// "assets/chr_sword.obj",
-// called when resource is loaded
-// function(object) {
-//   console.log("success", object);
-//   scene.add(object);
-// }
-// called when loading is in progresses
-// function(xhr) {
-//   console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-// },
-// called when loading has errors
-// function(error) {
-//   console.log("An error happened");
-// }
-// );
+  camera.position.set(0, 0.3, -1.5);
+
+  scene = new THREE.Scene();
+  camera.lookAt(scene.position);
+
+  createPortal(scene);
+
+  var geometry = new THREE.BoxBufferGeometry(0.2, 0.2, 0.2);
+  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = 0.1;
+
+  goal = new THREE.Object3D();
+  follow = new THREE.Object3D();
+  goal.position.z = -coronaSafetyDistance;
+  goal.add(camera);
+  scene.add(mesh);
+
+  var gridHelper = new THREE.GridHelper(40, 40);
+  scene.add(gridHelper);
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  keys = {
+    a: false,
+    s: false,
+    d: false,
+    w: false
+  };
+  document.body.addEventListener("keydown", function(e) {
+    var key = e.code.replace("Key", "").toLowerCase();
+    if (keys[key] !== undefined) keys[key] = true;
+  });
+  document.body.addEventListener("keyup", function(e) {
+    var key = e.code.replace("Key", "").toLowerCase();
+    if (keys[key] !== undefined) keys[key] = false;
+  });
+}
+
+function createPortal(scene) {
+  const portalMaterial = new THREE.MeshBasicMaterial({ color: 0xe0e0e0 });
+
+  const portalSide = new THREE.BoxBufferGeometry(0.3, 1, 0.3);
+  const portalRightSideMesh = new THREE.Mesh(portalSide, portalMaterial);
+
+  portalRightSideMesh.position.z = 0.5;
+  portalRightSideMesh.position.y = 0.5;
+  scene.add(portalRightSideMesh);
+
+  const portalLeftSideMesh = new THREE.Mesh(portalSide, portalMaterial);
+  portalLeftSideMesh.position.z = 0.5;
+  portalLeftSideMesh.position.y = 0.5;
+  portalLeftSideMesh.position.x = 0.7;
+  scene.add(portalLeftSideMesh);
+
+  const portalTop = new THREE.BoxBufferGeometry(1, 0.3, 0.3);
+
+  const portalTopMesh = new THREE.Mesh(portalTop, portalMaterial);
+  portalTopMesh.position.z = 0.5;
+  portalTopMesh.position.y = 1.1;
+  portalTopMesh.position.x = 0.35;
+  scene.add(portalTopMesh);
+}
 
 function animate() {
   requestAnimationFrame(animate);
+
+  speed = 0.0;
+
+  if (keys.w) speed = 0.02;
+  else if (keys.s) speed = -0.02;
+
+  velocity += (speed - velocity) * 0.3;
+  mesh.translateZ(velocity);
+
+  if (keys.a) mesh.rotateY(0.05);
+  else if (keys.d) mesh.rotateY(-0.05);
+
+  a.lerp(mesh.position, 0.4);
+  b.copy(goal.position);
+
+  dir
+    .copy(a)
+    .sub(b)
+    .normalize();
+  const dis = a.distanceTo(b) - coronaSafetyDistance;
+  goal.position.addScaledVector(dir, dis);
+  //temp.setFromMatrixPosition(goal.matrixWorld);
+
+  //camera.position.lerp(temp, 0.2);
+  camera.lookAt(mesh.position);
+
   renderer.render(scene, camera);
 }
-animate();
